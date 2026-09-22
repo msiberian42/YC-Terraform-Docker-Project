f0882910b62931bc179c81376eedcb0c6d2c3148
Pasha <p-berezovsky42@yandex.ru>
2026-09-22 17:14:08 +0300

    vpc

---
+++ vpc/main.tf
@@ -0,0 +1,14 @@
+resource "yandex_vpc_network" "vpc_list" {
+  name = var.env_name
+}
+
+resource "yandex_vpc_subnet" "vpc_list" {
+  for_each = {
+    for subnet in var.subnets : subnet.zone => subnet
+  }
+
+  name           = "${var.env_name}-${each.key}"
+  zone           = each.value.zone
+  network_id     = yandex_vpc_network.vpc_list.id
+  v4_cidr_blocks = [each.value.cidr]
+}
---
+++ vpc/variables.tf
@@ -0,0 +1,34 @@
+variable "env_name" {
+  description = "Enviroment name"
+  type        = string
+}
+
+variable "subnets" {
+  description = "Subnets list"
+  type = list(object({
+    zone = string
+    cidr = string
+  }))
+}
+
+# Cloud vars
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
